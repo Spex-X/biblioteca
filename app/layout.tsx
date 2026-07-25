08db46c7a5901062082769b596e10896e150e52a
jhonne333332 <seu-email@example.com>
2026-07-24 22:15:30 -0400

    Adicionar pixel do Facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,34 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1622521109495208');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <img
+                height="1"
+                width="1"
+                style="display:none"
+                src="https://www.facebook.com/tr?id=1622521109495208&ev=PageView&noscript=1"
+              />
+            `,
+          }}
+        />
       </head>
       <body className={`${nunito.variable} ${fredoka.variable} min-h-screen`}>{children}</body>
     </html>
